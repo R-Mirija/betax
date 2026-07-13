4d6401c86283453635dfb5e32c456fa6f0ef2e42
R-Mirija <pro.mirija@gmail.com>
2026-07-13 11:46:40 +0300

    replace psycopg2 with psycopg2-binary

--- backend/app/db/db_helper.py
+++ backend/app/db/db_helper.py
@@ -10,23 +10,32 @@ load_dotenv(dotenv_path=env_path)
 class DBHelper:
     _instance = None
 
-    # singleton db_helper
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._instance._connection = None
-            cls._instance._dsn = {
-                "dbname": os.getenv("DB_NAME"),
-                "user": os.getenv("DB_USER"),
-                "password": os.getenv("DB_PASSWORD"),
-                "host": os.getenv("DB_HOST"),
-                "port": os.getenv("DB_PORT")
-            }
+            
+            database_url = os.getenv("DATABASE_URL")
+            
+            if database_url:
+                cls._instance._dsn = database_url
+            else:
+                cls._instance._dsn = (
+                    f"dbname={os.getenv('DB_NAME')} "
+                    f"user={os.getenv('DB_USER')} "
+                    f"password={os.getenv('DB_PASSWORD')} "
+                    f"host={os.getenv('DB_HOST')} "
+                    f"port={os.getenv('DB_PORT')}"
+                )
+                
         return cls._instance
 
     def get_connection(self):
         if self._connection is None or self._connection.closed:
-            self._connection = psycopg2.connect(**self._dsn)
+            if isinstance(self._dsn, dict):
+                self._connection = psycopg2.connect(**self._dsn)
+            else:
+                self._connection = psycopg2.connect(self._dsn)
         return self._connection
 
     def close_connection(self):
