cceda70b7d4d9999240037ac7cb2f2d7bf1a7ef0
R-Mirija <pro.mirija@gmail.com>
2026-07-13 11:11:44 +0300

    set api to localhost

--- frontend/lib/utils/dio_util.dart
+++ frontend/lib/utils/dio_util.dart
@@ -2,41 +2,44 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 
 // api
-const apiAuthority = 'https://accentually-boastless-lora.ngrok-free.dev';
+const apiAuthority = 'http://localhost:8000';
 const apiPrefix = '/';
 
 // http utils
 final dio =
-    Dio(
-        BaseOptions(
-          connectTimeout: const Duration(seconds: 10),
-          receiveTimeout: const Duration(seconds: 10),
-          baseUrl: '$apiAuthority$apiPrefix',
-          headers: {
-            Headers.contentTypeHeader: 'application/json',
-            'User-Agent': 'beTax/1.0',
-          },
-        ),
-      )
-      ..interceptors.add(
-        InterceptorsWrapper(
-          onRequest: (options, handler) {
-            debugPrint(
-              'REQUEST[${options.method}] => PATH: ${options.path} param : ${options.queryParameters}',
-            );
-            return handler.next(options);
-          },
-          onResponse: (response, handler) {
-            debugPrint(
-              'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}',
-            );
-            return handler.next(response);
-          },
-          onError: (err, handler) {
-            debugPrint(
-              'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
-            );
-            return handler.next(err);
-          },
-        ),
-      );
+Dio(
+  BaseOptions(
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 10),
+    baseUrl: '$apiAuthority$apiPrefix',
+    headers: {
+      Headers.contentTypeHeader: 'application/json',
+      'User-Agent': 'beTax/1.0',
+    },
+  ),
+)
+  ..interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) {
+        debugPrint(
+          'REQUEST[${options.method}] => PATH: ${options.path} param : ${options
+              .queryParameters}',
+        );
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        debugPrint(
+          'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions
+              .path}',
+        );
+        return handler.next(response);
+      },
+      onError: (err, handler) {
+        debugPrint(
+          'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions
+              .path}',
+        );
+        return handler.next(err);
+      },
+    ),
+  );
